9b0b69e86c5515d004dd91b83a910914086e7bc3
Fernanda Leite <dev.fernandaleite@gmail.com>
2023-09-09 11:40:19 -0300

    feat: create item reducers

---
+++ src/reducers/items/actions.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import { Item } from './reducer'
+
+export enum ActionTypes {
+  ADD_ITEM_TO_CART = 'ADD_ITEM_TO_CART',
+  INCREASE_ITEMS_CART = 'INCREASE_ITEMS_CART',
+  DECREASE_ITEMS_CART = 'DECREASE_ITEMS_CART',
+  REMOVE_ITEMS_CART = 'REMOVE_ITEMS_CART',
+}
+
+export function addItemToCartAction(item: Item) {
+  return {
+    type: ActionTypes.ADD_ITEM_TO_CART,
+    payload: {
+      item,
+    },
+  }
+}
+
+export function increaseItemsCartAction(itemId: string) {
+  return {
+    type: ActionTypes.REMOVE_ITEMS_CART,
+    payload: {
+      itemId,
+    },
+  }
+}
+
+export function decreaseItemsCartAction(itemId: string) {
+  return {
+    type: ActionTypes.DECREASE_ITEMS_CART,
+    payload: {
+      itemId,
+    },
+  }
+}
+
+export function removeItemsCartAction(itemId: string) {
+  return {
+    type: ActionTypes.REMOVE_ITEMS_CART,
+    payload: {
+      itemId,
+    },
+  }
+}
---
+++ src/reducers/items/reducer.ts
@@ -0,0 +1,66 @@
+import { produce } from 'immer'
+
+import { ActionTypes } from './actions'
+
+export interface Item {
+  id: string
+  name: string
+  amount: number
+  price: number
+}
+
+interface ItemsState {
+  items: Item[]
+}
+
+export function itemsReducer(state: ItemsState, action: any) {
+  switch (action.type) {
+    case ActionTypes.ADD_ITEM_TO_CART:
+      return produce(state, (draft) => {
+        draft.items.push(action.payload.item)
+      })
+    case ActionTypes.INCREASE_ITEMS_CART: {
+      const currentItemIndex = state.items.findIndex((item) => {
+        return item.id === action.payload.itemId
+      })
+
+      if (currentItemIndex < 0) {
+        return state
+      }
+
+      return produce(state, (draft) => {
+        draft.items[currentItemIndex].amount =
+          draft.items[currentItemIndex].amount + 1
+      })
+    }
+    case ActionTypes.DECREASE_ITEMS_CART: {
+      const currentItemIndex = state.items.findIndex((item) => {
+        return item.id === action.payload.itemId
+      })
+
+      if (currentItemIndex < 0) {
+        return state
+      }
+
+      return produce(state, (draft) => {
+        draft.items[currentItemIndex].amount =
+          draft.items[currentItemIndex].amount - 1
+      })
+    }
+    case ActionTypes.REMOVE_ITEMS_CART: {
+      const currentItemIndex = state.items.findIndex((item) => {
+        return item.id === action.payload.itemId
+      })
+
+      if (currentItemIndex < 0) {
+        return state
+      }
+
+      return produce(state, (draft) => {
+        draft.items.splice(currentItemIndex, 1)
+      })
+    }
+    default:
+      return state
+  }
+}
